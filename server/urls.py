df5ab7a0acd76f5405b0b4f52e760438c55dc94f
rahmonbelmamadsaloev <rahmonbelmamadsaloev@MacBook-Air-rahmonbelmamadsaloev.local>
2025-11-30 16:42:55 +0500

    inclub to urls

--- server/urls.py
+++ server/urls.py
@@ -22,4 +22,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('accounts/', include('accounts.urls')),
+    path('api/', include('api.urls'))
 ]
