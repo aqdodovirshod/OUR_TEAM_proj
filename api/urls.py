dc2d2c6347dcbfa3f1085f46f6799a3192fa418f
rahmonbelmamadsaloev <rahmonbelmamadsaloev@MacBook-Air-rahmonbelmamadsaloev.local>
2025-11-30 16:37:30 +0500

    update serializer and added views  and urls

---
+++ api/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from .views import TransactionListCreateView ,TransactionDetailView , ProfileView ,BalanceView,FilterTransactionView
+urlpatterns = [
+    path('transactions/', TransactionListCreateView.as_view()),
+    path('transactions/<int:pk>/', TransactionDetailView.as_view()),
+    path('profile/', ProfileView.as_view(), ),
+    path('balance/', BalanceView.as_view(), ),
+    path('transactions/filter/', FilterTransactionView.as_view()),
+]
