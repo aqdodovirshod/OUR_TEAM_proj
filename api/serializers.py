8c6210bee9ec05ef890653f20e94151103d192d6
rahmonbelmamadsaloev <rahmonbelmamadsaloev@MacBook-Air-rahmonbelmamadsaloev.local>
2025-11-30 20:37:40 +0500

    change model tranzaktion

--- api/serializers.py
+++ api/serializers.py
@@ -7,11 +7,12 @@ class TransactionSerializer(serializers.ModelSerializer):
     class Meta:
         model = Transaction
         fields = "__all__"
+        read_only_fields = ("user",) 
 
     def create(self, validated_data):
-        user = self.context['request'].user
-        transaction = Transaction.objects.create(user=user, **validated_data)
-        return transaction
+        validated_data['user'] = self.context['request'].user
+        return Transaction.objects.create(**validated_data)
+
 
 
 class UsersProfileSerializer(serializers.ModelSerializer):
